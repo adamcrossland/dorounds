04849e007292734b4318c656d028276893bc9648
Adam Crossland <adam.crossland@gmail.com>
2021-10-14 15:49:33 -0400

    add about page to deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 scp index.html root@rolld:/home/dorounds/index.html
 scp dorounds.css root@rolld:/home/dorounds/dorounds.css
 scp dorounds.js root@rolld:/home/dorounds/dorounds.js
+scp about.html root@rolld:/home/dorounds/about.html
